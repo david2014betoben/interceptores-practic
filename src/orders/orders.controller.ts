1cb57bcc27b63c8d2de13bf55a8959f57f0d2ebb
david2014betoben <davidgerardonunezrojas@gmail.com>
2026-09-23 18:06:38 -0400

    correccion en el controller

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -9,6 +9,14 @@ import {
 import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 import { CreateOrderDto } from './dto/create-order.dto.js';
 import { OrdersService } from './orders.service.js';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiRequestTimeoutResponse,
+} from '@nestjs/swagger';
+import { Order } from './entities/order.entity.js';
 
 @ApiTags('orders')
 @Controller('orders')
@@ -20,6 +28,7 @@ export class OrdersController {
     summary: 'Listar todos los pedidos',
     description: 'Obtiene la lista completa de pedidos registrados.',
   })
+  @ApiOkResponse({ type: [Order] })
   findAll() {
     return this.ordersService.findAll();
   }
@@ -29,6 +38,7 @@ export class OrdersController {
     summary: 'Generar reporte pesado',
     description: 'Genera un reporte que requiere un procesamiento prolongado.',
   })
+  @ApiRequestTimeoutResponse()
   generateHeavyReport() {
     return this.ordersService.generateHeavyReport();
   }
@@ -43,6 +53,8 @@ export class OrdersController {
     description: 'ID del pedido',
     example: 1,
   })
+  @ApiOkResponse({ type: Order })
+  @ApiNotFoundResponse()
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.ordersService.findOne(id);
   }
@@ -52,6 +64,8 @@ export class OrdersController {
     summary: 'Crear un pedido',
     description: 'Crea un nuevo pedido con los datos proporcionados.',
   })
+  @ApiCreatedResponse({ type: Order })
+  @ApiBadRequestResponse()
   create(@Body() dto: CreateOrderDto) {
     return this.ordersService.create(dto);
   }
